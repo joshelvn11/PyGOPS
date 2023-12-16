60c62c5c640e8950e9f997f02d633aaebb2f44bc
Josh Beaver <josh.elvn11@gmail.com>
2023-12-16 21:15:13 +0000

    first iteration of basic server functionality

--- server.py
+++ server.py
@@ -1,19 +1,19 @@
 import socket
 import threading
 
+# Define constants
+SERVER_ADDR = 'localhost'
+PORT = 5050
+ADDR = (SERVER_ADDR, PORT)
+HEADER = 64
+FORMAT = 'utf-8'
+
 # Create a socket object
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to a specific address and port
-server_address = ('localhost', 5555)
+server_address = (SERVER_ADDR, PORT)
 server_socket.bind(server_address)
-
-# Listen for incoming connections
-server_socket.listen(5)
-print(f"Chat server is listening on {server_address[0]}:{server_address[1]}")
-
-# List to store connected clients
-clients = []
 
 # Variable to track the application state
 application_running = True
@@ -21,57 +21,8 @@ application_running = True
 # Variable to track the server state
 server_running = True
 
-
-def handle_client(client_socket):
-    while True:
-        try:
-            # Receive data from the client
-            data = client_socket.recv(1024).decode()
-            if not data:
-                break  # If no data received, break the loop
-
-            # Check if the command is to shut down the server
-            if data == "/shutdown":
-                server_socket.close()
-                print("Server shutting down...")
-                global server_running
-                server_running = False
-                break
-
-            # Broadcast the message to all clients
-            broadcast(data, client_socket)
-        except Exception as e:
-            print(f"Error: {e}")
-            break
-
-    # Remove the client from the list and close the connection
-    clients.remove(client_socket)
-    client_socket.close()
-
-
-def broadcast(message, sender_socket):
-
-    for client in clients:
-        try:
-            # Send the message to all other clients
-            client.send(message.encode())
-        except:
-            # If sending fails, remove the client from the list
-            clients.remove(client)
-
-
-def server_worker():
-    while server_running:
-        # Accept a connection
-        client_socket, client_address = server_socket.accept()
-        print(f"Connection from {client_address}")
-
-        # Add the new client to the list
-        clients.append(client_socket)
-
-        # Create a new thread to handle the client
-        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
-        client_thread.start()
+# List to store connected clients
+clients = []
 
 
 def start_server():
@@ -101,15 +52,86 @@ def stop_server():
     print('Server stopped...')
 
 
+def server_worker():
+
+    # Listen for incoming connections
+    server_socket.listen(5)
+    print(f"Chat server is listening on {server_address[0]}:{server_address[1]}")
+
+    while server_running:
+
+        # Accept a connection
+        # Get the client socket object and client address and assign to variables
+        client_socket, client_address = server_socket.accept()  # Blocking code
+        print(f"Connection from {client_address}")
+
+        # Add the new client to the list
+        clients.append(client_socket)
+
+        # Create a new thread to handle the client
+        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+        client_thread.start()
+
+        # List the active threads / connections
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+
+def handle_client(client_socket):
+
+    connected = True
+
+    # Code will listen for data / messages from the client while they are connected
+    while connected:
+        try:
+
+            # Receive the header containing the message length and decode it using the UTF-8 format
+            data_length = client_socket.recv(HEADER).decode(FORMAT)
+            # Parse the value to an integer
+            data_length = int(data_length)
+
+            # Receive the message data from the client using the data length received from the header
+            # Blocking code - will not pass this line until data is received
+            data_message = client_socket.recv(data_length).decode(FORMAT)
+
+            # Print the message to the terminal
+            print(f"[{client_socket}] {data_message}")
+
+            # If no data received or if the disconnect message is received from the client close the connection
+            if not data_message or data_message == '!DISCONNECT':
+                connected = False
+
+            # Broadcast the message to all clients
+            broadcast(data_message, client_socket)
+
+        except Exception as e:
+            print(f"Error: {e}")
+            break
+
+    # Remove the client from the list and close the connection
+    clients.remove(client_socket)
+    client_socket.close()
+
+
+def broadcast(message, sender_socket):
+
+    for client in clients:
+        try:
+            # Send the message to all other clients
+            client.send(message.encode())
+        except:
+            # If sending fails, remove the client from the list
+            clients.remove(client)
+
+
 while application_running:
-    command = input('Enter a command:')
+    command = input('Enter a command: ')
 
     if command == 'server-start':
         start_server()
     elif command == 'server-stop':
         stop_server()
     elif command == 'quit':
-        pass
+        break
     else:
         print('Please enter a valid command')
 
