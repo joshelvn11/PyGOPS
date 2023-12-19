c3ba1f9f675c0c02bfce17513697b1d5bd9d76a8
Josh Beaver <josh.elvn11@gmail.com>
2023-12-19 10:26:47 +0000

    [FEAT] added game variables to the game class

--- game.py
+++ game.py
@@ -10,13 +10,28 @@ class Game:
         self.server_instance = server_instance
         self.game_id = ""
         self.players = {initial_client[0]: initial_client}
+        # List to hold player points
+        self.player_points = []
+        # Dictionary of player one's cards
+        self.player_one_cards = {'A♠': 1, '2♠': 2, '3♠': 3, '4♠': 4, '5♠': 5, '6♠': 6, '7♠': 7, '8♠': 8, '9♠': 9,
+                                 '10♠': 10, 'J♠': 11, 'Q♠': 12, 'K♠': 13}
+        # Dictionary of player two's cards
+        self.player_two_cards = {'A♣': 1, '2♣': 2, '3♣': 3, '4♣': 4, '5♣': 5, '6♣': 6, '7♣': 7, '8♣': 8, '9♣': 9,
+                                 '10♣': 10, 'J♣': 11, 'Q♣': 12, 'K♣': 13}
+        # Dictionary of game cards
+        self.game_cards = {'A♦': 1, '2♦': 2, '3♦': 3, '4♦': 4, '5♦': 5, '6♦': 6, '7♦': 7, '8♦': 8, '9♦': 9, '10♦': 10,
+                           'J♦': 11, 'Q♦': 12, 'K♦': 13}
+        # List to hold the game cards currently in play
+        self.play_stack = []
 
+    # Generates a unique game and ID and returns it bak to the server
     def generate_id(self):
         characters = string.ascii_letters + string.digits
         self.game_id = ''.join(random.choice(characters) for _ in range(6))
         self.game_id = self.game_id.upper()
         return self.game_id
 
+    # Adds the requested player to the game
     def add_player(self, player):
 
         # Add player to the game and start the game procedure if there is currently only one player in the game
@@ -31,3 +46,5 @@ class Game:
                                     player[0], 64)
         elif len(self.players) == 0:
             pass
+
+
